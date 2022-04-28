09435efc9a957b8d5b234b7e0804fd209e8258fb
ednavho <ednaho121@gmail.com>
2022-04-27 19:25:23 -0700

    added test file with lib

--- practice1.java
+++ practice1.java
@@ -1,1 +1,6 @@
+public class practice1 {
 
+    public static int multiply(int n, int m) {
+        return n ** m;
+    }
+}
---
+++ practice1Tester.java
@@ -0,0 +1,11 @@
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class practice1Tester {
+    
+    @Test
+    public void recip() {
+        assertEquals(10, practice1.multiply(5,2));
+    }
+}
